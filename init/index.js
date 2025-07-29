4b6026452f1728ee04051870142939247a1edc98
Vinit kashyap <vinitkumar10062004@gmail.com>
2025-07-29 13:08:26 +0530

    added some styling

--- init/index.js
+++ init/index.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
-
 const mongo_url = 'mongodb://127.0.0.1:27017/wanderlust';
+
 main()
     .then(() => {
         console.log("connected to mongo successfully..");
